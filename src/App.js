7ab4e5774ac61187297d4b84260da5e48bd0f9b7
anoob09 <aswarnkar.anup.swarnkar@gmail.com>
2021-01-15 13:51:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useTable, useSortBy } from "react-table";
+import "./App.css";
 
 function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "TEAM",
+        accessor: "team", // accessor is the "key" in the data
+      },
+      {
+        Header: "GROUP",
+        accessor: "group",
+      },
+      {
+        Header: "SPI",
+        accessor: "spi",
+      },
+      {
+        Header: "OFF.",
+        accessor: "off",
+      },
+      {
+        Header: "DEF.",
+        accessor: "def",
+      },
+      {
+        Header: "MAKE ROUND OF 16",
+        accessor: "ro16",
+      },
+      {
+        Header: "MAKE QTR-FINALS",
+        accessor: "qf",
+      },
+      {
+        Header: "MAKE SEMIFINALS",
+        accessor: "sf",
+      },
+      {
+        Header: "WIN WORLD CUP",
+        accessor: "win",
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(
+    () => [
+      {
+        team: "Argentina",
+        group: "D",
+        spi: 98.3,
+        off: 1.5,
+        def: 1.9,
+        ro16: "Yes",
+        qf: "78.4%",
+        sf: "42.1%",
+        win: "19.4%",
+      },
+      {
+        team: "Argentina",
+        group: "D",
+        spi: 98.3,
+        off: 1.5,
+        def: 1.9,
+        ro16: "Yes",
+        qf: "78.4%",
+        sf: "42.1%",
+        win: "19.4%",
+      },
+      {
+        team: "Argentina",
+        group: "D",
+        spi: 98.3,
+        off: 1.5,
+        def: 1.9,
+        ro16: "Yes",
+        qf: "78.4%",
+        sf: "42.1%",
+        win: "19.4%",
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+
+  function getRowClassName(cell) {
+    if (cell.column.Header === "TEAM") return "team-name";
+    else if (cell.column.Header === "SPI") return "spi-rating";
+    else return "number";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table className="standings-table" {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th className="cell" {...column.getHeaderProps()}>
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    className={`p-5 ${getRowClassName(cell)} cell`}
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
